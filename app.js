096101621a35c40b3c1e600e854d8e6990b0e050
Saulo Andrade Vilela <sauloandvilela@gmail.com>
2025-02-24 11:52:07 -0300

    alterando numero limite para 100

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 
 tentativas = 1;
@@ -12,7 +12,7 @@ function exibirTextoNaTela (tag, texto) {
 
 function exibirMensagemInicial(){
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número de 1 a 10');
+    exibirTextoNaTela('p', `Escolha um número de 1 a ${numeroLimite}`);
 }
 
 exibirMensagemInicial()
